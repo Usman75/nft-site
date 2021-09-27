f40367771ddee4ad11b74e2c594d44b0dfeae941
Lyndi Castrejon <lyndic@protonmail.com>
2021-09-26 19:20:43 -0500

    Add logic for wallet connection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './styles/App.css';
+import React, { useEffect, useState } from "react";
 import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
+import './styles/App.css';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -9,10 +9,61 @@ const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkWalletConnection = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have MetaMask installed!");
+      return;
+    } else {
+      console.log("We have the ethereum object ", ethereum);
+    }
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+  
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account: ", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  useEffect(() => {
+    checkWalletConnection();
+  }, [])
+
   // Render Methods
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
+    </button>
+  );
+
+  const renderMintUI = () => (
+    <button onClick={null} className="cta-button connect-wallet-button">
+      Mint NFT
     </button>
   );
 
@@ -20,11 +71,11 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">My NFT Collection</p>
+          <p className="header gradient-text">EpicNFT Collection</p>
           <p className="sub-text">
-            Each unique. Each beautiful. Discover your NFT today.
+            Hundreds of unique personalities. Discover your blockchain alter-ego today.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount === "" ? renderNotConnectedContainer() : renderMintUI()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
